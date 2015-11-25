06153015c2337d659d110ee95afdcce11eb06991
あまみや ゆうこ <1@1234.sh>
2015-11-25 09:25:50 +0800

    Bump to dotnet5.5

--- src/CodeComb.Testing/TestApplicationEnvironment.cs
+++ src/CodeComb.Testing/TestApplicationEnvironment.cs
@@ -33,30 +33,12 @@ namespace CodeComb.Testing
 
         public string ApplicationBasePath { get; }
 
-        public string Configuration
-        {
-            get
-            {
-                return _original.Configuration;
-            }
-        }
-
         public FrameworkName RuntimeFramework
         {
             get
             {
                 return _original.RuntimeFramework;
             }
-        }
-
-        public object GetData(string name)
-        {
-            return _original.GetData(name);
-        }
-
-        public void SetData(string name, object value)
-        {
-            _original.SetData(name, value);
         }
     }
 }
